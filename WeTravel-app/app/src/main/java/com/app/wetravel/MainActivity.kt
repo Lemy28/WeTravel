8afc757561075c7c433a747e61f4e830fe957820
1347480050 <1347480050@qq.com>
2023-06-24 15:44:57 +0800

    实现推送
    实现了项目接受后台推送消息

--- WeTravel-app/app/src/main/java/com/app/wetravel/MainActivity.kt
+++ WeTravel-app/app/src/main/java/com/app/wetravel/MainActivity.kt
@@ -4,6 +4,7 @@ import OkHttpTest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+
 import com.app.wetravel.databinding.ActivityMainBinding
 import com.app.wetravel.databinding.BottomFragmentBinding
 
@@ -11,10 +12,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-    val Okhttp =OkHttpTest(13220324,123)
+    //val Okhttp =OkHttpTest(13220324,123)
 
 
 }
