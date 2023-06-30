26a52686d96373e870ac6f43856e47caa1051644
1347480050 <1347480050@qq.com>
2023-06-30 17:00:57 +0800

    1

--- WeTravel-app/app/src/main/java/com/app/wetravel/travelApplication.java
+++ WeTravel-app/app/src/main/java/com/app/wetravel/travelApplication.java
@@ -3,15 +3,28 @@ package com.app.wetravel;
 import android.app.Application;
 import android.content.Context;
 
-import cn.jpush.android.api.JPushInterface;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.maps.MapsInitializer;
+import com.amap.api.services.core.ServiceSettings;
 
-public class travelApplication extends Application {
+
+//应用注册，权限申请
+public class TravelApplication extends Application {
+
     @Override
     public void onCreate() {
         super.onCreate();
-        JPushInterface.setDebugMode(true);
-        // 初始化 JPush
-        JPushInterface.init(this);
+
+        Context context = this;
+        //定位隐私政策同意
+        AMapLocationClient.updatePrivacyShow(context,true,true);
+        AMapLocationClient.updatePrivacyAgree(context,true);
+        //地图隐私政策同意
+        MapsInitializer.updatePrivacyShow(context,true,true);
+        MapsInitializer.updatePrivacyAgree(context,true);
+        //搜索隐私政策同意
+        ServiceSettings.updatePrivacyShow(context,true,true);
+        ServiceSettings.updatePrivacyAgree(context,true);
 
     }
 }
